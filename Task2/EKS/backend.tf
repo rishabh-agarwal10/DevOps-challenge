1a96490282c87a51b9fea02ac563b2488b83d16f
rishabh-agarwal10 <90672095+rishabh-agarwal10@users.noreply.github.com>
2025-04-19 12:34:49 +0530

    Delete Task2/EKS directory

--- Task2/EKS/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-  }
-}
--- Task2/EKS/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-region         = "us-east-1"
-key_pair_name  = "your-ec2-keypair"
