d3c7f1ddff29bedd8c3ebb74738ff5b05cd66551
rishabh-agarwal10 <90672095+rishabh-agarwal10@users.noreply.github.com>
2025-04-19 12:13:15 +0530

    Create s3.tf

---
+++ Task Extra/s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
